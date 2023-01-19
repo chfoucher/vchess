c2b4846caa59851e2c5fa4a471b3f6e5aec34809
Christophe Foucher <pele.foucher@free.fr>
2023-01-19 16:43:53 +0100

    Renommer HelloWorld en Game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import Game from "./components/Game.vue";
 import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Game msg="You did it!" />
     </div>
   </header>
 
