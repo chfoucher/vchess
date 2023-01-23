9a3a3277a52631a8657dad08a63fc63784e242f4
Christophe Foucher <pele.foucher@free.fr>
2023-01-23 11:40:40 +0100

    Suppression de test inutile, ajout test initBoard

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 /*global describe, it */
 import { expect } from "chai";
-import { initBoard } from "../src/utils/rules.js";
-
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      expect([1, 2, 3].indexOf(4)).to.equal(-1); // assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+import { initBoard, PAWN, ROOK } from "../src/utils/rules.js";
 
 describe("rules", function () {
   describe("initBoard", function () {
     it("should return the board layout at game start", function () {
       const board = initBoard();
       expect(board[0][0].black).to.be.false;
+      expect(board[0][1].black).to.be.true;
+      expect(board[2][3].r).to.equal(2);
+      expect(board[2][3].c).to.equal(3);
+      expect(board[0][0]).to.have.property("piece");
+      expect(board[0][0].piece).to.deep.equal({ type: ROOK, black: true });
+      expect(board[6][1]).to.have.property("piece");
+      expect(board[6][1].piece).to.deep.equal({ type: PAWN, black: false });
+      expect(board[2][0]).to.not.have.property("piece");
     });
   });
 });
