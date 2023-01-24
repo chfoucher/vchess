b7cb8d323837d9ff5decded20a2add379c6c05da
chfoucher <pele.foucher@free.fr>
2023-01-24 15:11:10 +0100

    Suppression de fichiers inutiles

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,6 +1,37 @@
 <script setup>
-import * as utils from "../utils";
 import { computed } from "vue";
+
+import viseur_noir from "../assets/viseur_noir.svg";
+import viseur_blanc from "../assets/viseur_blanc.svg";
+import Chess_hdt45 from "../assets/Chess_hdt45.svg";
+import Chess_plt45 from "../assets/Chess_plt45.svg";
+import Chess_mdt45 from "../assets/Chess_mdt45.svg";
+import Chess_rlt45 from "../assets/Chess_rlt45.svg";
+import Chess_fdt45 from "../assets/Chess_fdt45.svg";
+import Chess_klt45 from "../assets/Chess_klt45.svg";
+import Chess_Bdt45 from "../assets/Chess_Bdt45.svg";
+import Chess_blt45 from "../assets/Chess_blt45.svg";
+import Chess_Ndt45 from "../assets/Chess_Ndt45.svg";
+import Chess_nlt45 from "../assets/Chess_nlt45.svg";
+import Chess_gdt45 from "../assets/Chess_gdt45.svg";
+import Chess_qlt45 from "../assets/Chess_qlt45.svg";
+const images = [
+  viseur_noir,
+  viseur_blanc,
+  Chess_hdt45,
+  Chess_plt45,
+  Chess_mdt45,
+  Chess_rlt45,
+  Chess_fdt45,
+  Chess_klt45,
+  Chess_Bdt45,
+  Chess_blt45,
+  Chess_Ndt45,
+  Chess_nlt45,
+  Chess_gdt45,
+  Chess_qlt45,
+];
+
 const props = defineProps({
   black: Boolean,
   c: Number,
@@ -10,9 +41,9 @@ const props = defineProps({
 });
 const imgSrc = computed(() => {
   const i = 2 * props.piece.type;
-  const j = (props.piece.couleur === "noir") ? 0 : 1;
+  const j = props.piece.couleur === "noir" ? 0 : 1;
   const k = i + j;
-  return utils.images[k];
+  return images[k];
 });
 const emit = defineEmits(["click"]);
 
