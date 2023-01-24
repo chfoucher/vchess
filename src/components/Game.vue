4b3209bfd1c798676fa2acad413abbe6ecb23c6e
chfoucher <pele.foucher@free.fr>
2023-01-24 14:22:48 +0100

    Fonction d'annulation

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -89,27 +89,35 @@ function initBoard() {
 }
 
 function showStatus() {
-    if (selection) {
-        message.value = "Sélectionne la destination";
-    } else {
-      message.value = `Aux ${joueurActif}s de jouer`;
-    }
+  if (selection) {
+    message.value = "Sélectionne la destination";
+  } else {
+    message.value = `Aux ${joueurActif}s de jouer`;
+  }
 }
 
 function onClick(caseChoisie) {
-  console.log(selection);
   if (selection) {
     if (destinationAutorisee(caseChoisie.r, caseChoisie.c)) {
       historique.value.push({
-                origine: {r: selection.r, c:selection.c, piece: board.value[selection.r][selection.c].piece},
-                destination: {r: caseChoisie.r, c: caseChoisie.c, piece: board.value[caseChoisie.r][caseChoisie.c].piece}
-            });
-      const adversaire = (joueurActif === BLANC)?NOIR:BLANC;
+        origine: {
+          r: selection.r,
+          c: selection.c,
+          piece: board.value[selection.r][selection.c].piece,
+        },
+        destination: {
+          r: caseChoisie.r,
+          c: caseChoisie.c,
+          piece: board.value[caseChoisie.r][caseChoisie.c].piece,
+        },
+      });
+      const adversaire = joueurActif === BLANC ? NOIR : BLANC;
       retireOrigine(joueurActif, selection.r, selection.c);
-      if (board.value[caseChoisie.r][caseChoisie.c].piece) retireOrigine(adversaire, caseChoisie.r, caseChoisie.c);
+      if (board.value[caseChoisie.r][caseChoisie.c].piece)
+        retireOrigine(adversaire, caseChoisie.r, caseChoisie.c);
       ajouteOrigine(joueurActif, caseChoisie.r, caseChoisie.c);
       board.value[caseChoisie.r][caseChoisie.c].piece =
-      board.value[selection.r][selection.c].piece;
+        board.value[selection.r][selection.c].piece;
       promotion(caseChoisie);
       roque(selection.r, selection.c, caseChoisie.r, caseChoisie.c);
       board.value[selection.r][selection.c].piece = null;
@@ -127,222 +135,321 @@ function onClick(caseChoisie) {
   showStatus();
 }
 
-function promotion(destination) {
-    const piece = board.value[destination.r][destination.c].piece;
-    if (piece.type === PION) {
-        if ((piece.couleur === NOIR && destination.r === 7) ||
-         (piece.couleur === BLANC && destination.r === 0)) {
-             const nouvPiece = {type: REINE, couleur: piece.couleur};
-             board.value[destination.r][destination.c].piece = nouvPiece;
-         }
+function onAnnule() {
+  const mouvement = historique.value.pop();
+  if (mouvement == null) return;
+  const orig = mouvement.origine; // {row, col, piece}
+  const dst = mouvement.destination;
+  const couleur = orig.piece.couleur;
+  const roque = orig.piece.type === ROI && Math.abs(dst.c - orig.c) == 2;
+  if (roque) {
+    if (dst.c - orig.c > 0) {
+      retireOrigine(couleur, dst.r, 5);
+      ajouteOrigine(couleur, dst.r, 7);
+      board.value[dst.r][7].piece = board.value[dst.r][5].piece;
+      board.value[dst.r][5].piece = null;
+    } else {
+      retireOrigine(couleur, dst.r, 3);
+      ajouteOrigine(couleur, dst.r, 0);
+      board.value[dst.r][0].piece = board.value[dst.r][3].piece;
+      board.value[dst.r][3].piece = null;
     }
+  }
+  retireOrigine(couleur, dst.r, dst.c);
+  if (dst.piece) ajouteOrigine(dst.piece.couleur, dst.r, dst.c);
+  ajouteOrigine(couleur, orig.r, orig.c);
+  board.value[orig.r][orig.c].piece = orig.piece;
+  board.value[dst.r][dst.c].piece = dst.piece;
+  joueurActif = joueurActif === BLANC ? NOIR : BLANC;
+  calculeMouvements();
+  showStatus();
+}
+
+function promotion(destination) {
+  const piece = board.value[destination.r][destination.c].piece;
+  if (piece.type === PION) {
+    if (
+      (piece.couleur === NOIR && destination.r === 7) ||
+      (piece.couleur === BLANC && destination.r === 0)
+    ) {
+      const nouvPiece = { type: REINE, couleur: piece.couleur };
+      board.value[destination.r][destination.c].piece = nouvPiece;
+    }
+  }
 }
 
 function roque(or, oc, dr, dc) {
-    const couleur = board.value[or][oc].piece.couleur;
-    const row = or;
-    let origCol, destCol;
-    let roqueEnCours = false;
-    if ((oc == 4) && (board.value[or][oc].piece.type === ROI)) {
-        if (dc == 2) {
-            origCol = 0;
-            destCol = dc + 1;
-            roqueEnCours = true;
-        }
-        if (dc == 6) {
-            origCol = 7;
-            destCol = dc - 1;
-            roqueEnCours = true;
-        }
-        if (roqueEnCours) {
-            board.value[row][destCol].piece = board.value[row][origCol].piece;
-            board.value[row][origCol].piece = null;
-            retireOrigine(couleur, row, origCol);
-            ajouteOrigine(couleur, row, destCol);
-        }
+  const couleur = board.value[or][oc].piece.couleur;
+  const row = or;
+  let origCol, destCol;
+  let roqueEnCours = false;
+  if (oc == 4 && board.value[or][oc].piece.type === ROI) {
+    if (dc == 2) {
+      origCol = 0;
+      destCol = dc + 1;
+      roqueEnCours = true;
     }
+    if (dc == 6) {
+      origCol = 7;
+      destCol = dc - 1;
+      roqueEnCours = true;
+    }
+    if (roqueEnCours) {
+      board.value[row][destCol].piece = board.value[row][origCol].piece;
+      board.value[row][origCol].piece = null;
+      retireOrigine(couleur, row, origCol);
+      ajouteOrigine(couleur, row, destCol);
+    }
+  }
 }
 
 function ajouteOrigine(couleur, r, c) {
-    mvtsPossibles[couleur].push({origine: { r, c }, destinations:[]});
+  mvtsPossibles[couleur].push({ origine: { r, c }, destinations: [] });
 }
 
 function retireOrigine(couleur, r, c) {
-    const mvts = mvtsPossibles[couleur];
-    for (let i = 0; i < mvts.length; i++) {
-        if (mvts[i].origine.r === r && mvts[i].origine.c === c) {
-            mvts.splice(i, 1);
-            break;
-        }
+  const mvts = mvtsPossibles[couleur];
+  for (let i = 0; i < mvts.length; i++) {
+    if (mvts[i].origine.r === r && mvts[i].origine.c === c) {
+      mvts.splice(i, 1);
+      break;
     }
+  }
 }
 
 function calculeMouvementsPion(r, c) {
-    const resultat = [];
-    let dr, dc;
-    const direction = (joueurActif) === BLANC?-1:1;
-    // Avance de 1
-    dr = r + direction;
-    dc = c;
-    if (dr <= 7 && dr >= 0 && !(board.value[dr][dc].piece)) resultat.push({ r: dr, c: dc });
-    // Avance de 2
-    const startingRow = (joueurActif) === BLANC?6:1;
-    if (r === startingRow) {
-        dr = r + direction * 2;
-        if (dr <= 7 && dr >= 0 && !(board.value[dr][dc].piece)) resultat.push({ r: dr, c: dc });
-    }
-    // Mange à gauche
-    dr = r + direction;
-    dc = c - 1;
-    if (dr <= 7 && dr >= 0 && dc >= 0 && (board.value[dr][dc].piece) &&
-        board.value[dr][dc].piece.couleur != joueurActif)
-        resultat.push({ r: dr, c: dc });
-    // Mange à droite
-    dc = c + 1;
-    if (dr <= 7 && dr >= 0 && dc <= 7 && (board.value[dr][dc].piece) &&
-        board.value[dr][dc].piece.couleur != joueurActif)
-        resultat.push({ r: dr, c: dc });
-    return resultat;
+  const resultat = [];
+  let dr, dc;
+  const direction = joueurActif === BLANC ? -1 : 1;
+  // Avance de 1
+  dr = r + direction;
+  dc = c;
+  if (dr <= 7 && dr >= 0 && !board.value[dr][dc].piece)
+    resultat.push({ r: dr, c: dc });
+  // Avance de 2
+  const startingRow = joueurActif === BLANC ? 6 : 1;
+  if (r === startingRow) {
+    dr = r + direction * 2;
+    if (dr <= 7 && dr >= 0 && !board.value[dr][dc].piece)
+      resultat.push({ r: dr, c: dc });
+  }
+  // Mange à gauche
+  dr = r + direction;
+  dc = c - 1;
+  if (
+    dr <= 7 &&
+    dr >= 0 &&
+    dc >= 0 &&
+    board.value[dr][dc].piece &&
+    board.value[dr][dc].piece.couleur != joueurActif
+  )
+    resultat.push({ r: dr, c: dc });
+  // Mange à droite
+  dc = c + 1;
+  if (
+    dr <= 7 &&
+    dr >= 0 &&
+    dc <= 7 &&
+    board.value[dr][dc].piece &&
+    board.value[dr][dc].piece.couleur != joueurActif
+  )
+    resultat.push({ r: dr, c: dc });
+  return resultat;
 }
 
 function calculeMouvementsCercle(mvts, r, c) {
-    const resultat = [];
-    let dr, dc;
-    for (const mvt of mvts) {
-        dr = r + mvt[0];
-        dc = c + mvt[1];
-        if (dr <= 7 && dr >= 0 && dc <= 7 && dc >= 0) {
-            if (!(board.value[dr][dc].piece) || (board.value[dr][dc].piece.couleur != joueurActif)) {
-                resultat.push({ r: dr, c: dc });
-            }
-        }
+  const resultat = [];
+  let dr, dc;
+  for (const mvt of mvts) {
+    dr = r + mvt[0];
+    dc = c + mvt[1];
+    if (dr <= 7 && dr >= 0 && dc <= 7 && dc >= 0) {
+      if (
+        !board.value[dr][dc].piece ||
+        board.value[dr][dc].piece.couleur != joueurActif
+      ) {
+        resultat.push({ r: dr, c: dc });
+      }
     }
-    return resultat;
+  }
+  return resultat;
 }
 
 function calculeMouvementsCavalier(r, c) {
-    return calculeMouvementsCercle(
-        [[1, 2], [2, 1], [-1, 2], [2, -1],
-         [1, -2], [-2, 1], [-1, -2], [-2, -1]],
-        r, c);
+  return calculeMouvementsCercle(
+    [
+      [1, 2],
+      [2, 1],
+      [-1, 2],
+      [2, -1],
+      [1, -2],
+      [-2, 1],
+      [-1, -2],
+      [-2, -1],
+    ],
+    r,
+    c
+  );
 }
 
 function calculeRoque(r, c) {
-    const resultat = [];
-    const startingRow = (joueurActif) === BLANC?7:0;
-    if (r == startingRow && c == 4 && !board.value[r][c+1].piece && !board.value[r][c+2].piece) {
-        resultat.push({ r, c: c + 2 });
-    }
-    if (r == startingRow && c == 4 && !board.value[r][c-1].piece && !board.value[r][c-2].piece && !board.value[r][c-3].piece) {
-        resultat.push({ r, c: c - 2 });
-    }
-    return resultat;
+  const resultat = [];
+  const startingRow = joueurActif === BLANC ? 7 : 0;
+  if (
+    r == startingRow &&
+    c == 4 &&
+    !board.value[r][c + 1].piece &&
+    !board.value[r][c + 2].piece
+  ) {
+    resultat.push({ r, c: c + 2 });
+  }
+  if (
+    r == startingRow &&
+    c == 4 &&
+    !board.value[r][c - 1].piece &&
+    !board.value[r][c - 2].piece &&
+    !board.value[r][c - 3].piece
+  ) {
+    resultat.push({ r, c: c - 2 });
+  }
+  return resultat;
 }
 
 function calculeMouvementsRoi(r, c) {
-    const mvtsNormaux = calculeMouvementsCercle(
-        [[1, 0], [1, 1], [1, -1], [0, -1],
-        [-1, 0], [-1, 1], [-1, -1], [0, 1]],
-        r, c);
-    const mvtsRoque = calculeRoque(r, c);
-    return mvtsNormaux.concat(mvtsRoque);
+  const mvtsNormaux = calculeMouvementsCercle(
+    [
+      [1, 0],
+      [1, 1],
+      [1, -1],
+      [0, -1],
+      [-1, 0],
+      [-1, 1],
+      [-1, -1],
+      [0, 1],
+    ],
+    r,
+    c
+  );
+  const mvtsRoque = calculeRoque(r, c);
+  return mvtsNormaux.concat(mvtsRoque);
 }
 
 function calculeMouvementsLigne(mvts, r, c) {
-    const resultat = [];
-    var dr, dc;
-    for (const mvt of mvts) {
-        dr = r + mvt[0];
-        dc = c + mvt[1];
-        while (dr <= 7 && dr >= 0 && dc <= 7 && dc >= 0) {
-            if (board.value[dr][dc].piece) {
-                if (board.value[dr][dc].piece.couleur != joueurActif) {
-                    // Prise
-                    resultat.push({ r: dr, c: dc });
-                }
-                break; // Fin de la trajectoire
-            } else { // Simple mouvement
-                resultat.push({ r: dr, c: dc });
-            }
-            dr += mvt[0];
-            dc += mvt[1];
+  const resultat = [];
+  var dr, dc;
+  for (const mvt of mvts) {
+    dr = r + mvt[0];
+    dc = c + mvt[1];
+    while (dr <= 7 && dr >= 0 && dc <= 7 && dc >= 0) {
+      if (board.value[dr][dc].piece) {
+        if (board.value[dr][dc].piece.couleur != joueurActif) {
+          // Prise
+          resultat.push({ r: dr, c: dc });
         }
+        break; // Fin de la trajectoire
+      } else {
+        // Simple mouvement
+        resultat.push({ r: dr, c: dc });
+      }
+      dr += mvt[0];
+      dc += mvt[1];
     }
-    return resultat;
+  }
+  return resultat;
 }
 
 function calculeMouvementsFou(r, c) {
-    return calculeMouvementsLigne([[1, 1], [1, -1], [-1, -1], [-1, 1]], r, c);
+  return calculeMouvementsLigne(
+    [
+      [1, 1],
+      [1, -1],
+      [-1, -1],
+      [-1, 1],
+    ],
+    r,
+    c
+  );
 }
 
 function calculeMouvementsTour(r, c) {
-    return calculeMouvementsLigne([[0, 1], [0, -1], [-1, 0], [1, 0]], r, c);
+  return calculeMouvementsLigne(
+    [
+      [0, 1],
+      [0, -1],
+      [-1, 0],
+      [1, 0],
+    ],
+    r,
+    c
+  );
 }
 
 function calculeMouvementsReine(r, c) {
-    const mvtsFou = calculeMouvementsFou(r, c);
-    const mvtsTour = calculeMouvementsTour(r, c);
-    return mvtsFou.concat(mvtsTour);
+  const mvtsFou = calculeMouvementsFou(r, c);
+  const mvtsTour = calculeMouvementsTour(r, c);
+  return mvtsFou.concat(mvtsTour);
 }
 
 function calculeMouvementsOrigine(r, c) {
-    const piece = board.value[r][c].piece;
-    var resultat = [];
-    switch (piece.type) {
-        case PION:
-            resultat = calculeMouvementsPion(r, c);
-            break;
-        case CHEVAL:
-            resultat = calculeMouvementsCavalier(r, c);
-            break;
-        case FOU:
-            resultat = calculeMouvementsFou(r, c);
-            break;
-        case TOUR:
-            resultat = calculeMouvementsTour(r, c);
-            break;
-        case REINE:
-            resultat = calculeMouvementsReine(r, c);
-            break;
-        case ROI:
-            resultat = calculeMouvementsRoi(r, c);
-            break;
-    }
-    return resultat;
+  const piece = board.value[r][c].piece;
+  var resultat = [];
+  switch (piece.type) {
+    case PION:
+      resultat = calculeMouvementsPion(r, c);
+      break;
+    case CHEVAL:
+      resultat = calculeMouvementsCavalier(r, c);
+      break;
+    case FOU:
+      resultat = calculeMouvementsFou(r, c);
+      break;
+    case TOUR:
+      resultat = calculeMouvementsTour(r, c);
+      break;
+    case REINE:
+      resultat = calculeMouvementsReine(r, c);
+      break;
+    case ROI:
+      resultat = calculeMouvementsRoi(r, c);
+      break;
+  }
+  return resultat;
 }
 
 function calculeMouvements() {
-    const possibles = mvtsPossibles[joueurActif];
-    for (const mouvement of possibles) {
-        mouvement.destinations = calculeMouvementsOrigine(mouvement.origine.r, mouvement.origine.c);
-    }
+  const possibles = mvtsPossibles[joueurActif];
+  for (const mouvement of possibles) {
+    mouvement.destinations = calculeMouvementsOrigine(
+      mouvement.origine.r,
+      mouvement.origine.c
+    );
+  }
 }
 
 function origineAutorisee({ r, c }) {
-  console.log("origineAutorisee ?", joueurActif)
-  console.log(mvtsPossibles);
-    const cases = mvtsPossibles[joueurActif];
-    for (let i = 0; i < cases.length; i++) {
-        if (cases[i].origine.r === r && cases[i].origine.c === c && cases[i].destinations.length) {
-            indexOrigine = i;
-            console.log("Oui");
-            return true;
-        }
+  const cases = mvtsPossibles[joueurActif];
+  for (let i = 0; i < cases.length; i++) {
+    if (
+      cases[i].origine.r === r &&
+      cases[i].origine.c === c &&
+      cases[i].destinations.length
+    ) {
+      indexOrigine = i;
+      return true;
     }
-    console.log("Non !");
-    return false;
+  }
+  return false;
 }
 
 function destinationAutorisee(dr, dc) {
-    const destinations = mvtsPossibles[joueurActif][indexOrigine].destinations;
-    for (let i = 0; i < destinations.length; i++) {
-        if (destinations[i].r === dr && destinations[i].c === dc) {
-            return true;
-        }
+  const destinations = mvtsPossibles[joueurActif][indexOrigine].destinations;
+  for (let i = 0; i < destinations.length; i++) {
+    if (destinations[i].r === dr && destinations[i].c === dc) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
-
 </script>
 
 <template>
@@ -360,8 +467,10 @@ function destinationAutorisee(dr, dc) {
   <div>
     <button id="btnNouveau">Nouvelle partie</button>
     <button id="btnEnregistre">Enregistre partie</button>
-    <button id="btnAnnule" :disabled="annulationDesactivee">Annule</button>
-    {{  message }}
+    <button id="btnAnnule" :disabled="annulationDesactivee" @click="onAnnule">
+      Annule
+    </button>
+    {{ message }}
   </div>
 </template>
 
